4991b4e05142572d15c7ec1c3c41c76a8d7b85de
06aga06m <06aga06m@mail.ru>
2018-11-08 12:32:07 +0300

    added home-page and some bootstrap

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -120,9 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_prod")
+
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
+
 
 AUTH_USER_MODEL = 'shop.User'
--- shop/views.py
+++ shop/views.py
@@ -1,19 +1,25 @@
 import os
-
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
 from django.contrib import auth
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect, get_object_or_404
-
+from django.contrib.auth.decorators import login_required
 # from shop import settings
 from shop.form import *
 from shop.models import *
 
+
 def index(request):
     user = auth.get_user(request)
+
+    products_Images = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    products_images_phones = products_Images.filter(product__category__id=1)
+    products_images_laptops = products_Images.filter(product__category__id=2)
+
     if user.is_anonymous:
         return render(request, 'shop//index/index.html')
     else:
-        return render(request, 'shop/index/index.html', {'username': auth.get_user(request)})
+        return render(request, 'shop/index/index.html', locals())
 
 
 def login(request):
